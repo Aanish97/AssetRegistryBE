1e7af5e377d86358f7aea5f9eb1cd4b731b7c4f6
Muhammad Hamza Ashraf <mhamza.141097@gmail.com>
2022-12-18 03:35:52 +0500

    Field Boundary Registration, geo id and respective S2 Cell Tokens.

--- app.py
+++ app.py
@@ -1,82 +1,19 @@
-import os
 import json
-import hashlib
 import fiona
 import geopandas as gpd
-from flask import Flask, jsonify, request
+from flask import jsonify, request
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from shapely.wkt import loads
-import s2sphere as s2
+from db import app, db
+from s2_service import S2Service
+from utils import Utils
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Flask(__name__)
-app.config.from_object(os.getenv('APP_SETTINGS'))
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 from db.models import geoIdsModel, s2CellTokensModel, cellsGeosMiddleModel
 
 migrate = Migrate(app, db)
-
-
-@app.route('/generate-geo-id', methods=['POST'])
-def generate_geo_ids():
-    """
-    each list of `s2_index__L20_list` will always have a unique GEO_ID
-    """
-
-    data = json.loads(request.data.decode('utf-8'))
-    s2_tokens = data.get('s2_tokens')
-
-    s2_tuple = tuple(s2_tokens)
-    m = hashlib.sha256()
-
-    # encoding the s2 tokens list
-    for s in s2_tuple:
-        m.update(s.encode())
-    geo_id = m.hexdigest()  # <-- geoid
-
-    # order matters
-    return jsonify({
-        "GEO ID": geo_id
-    })
-
-
-@app.route('/wkt-to-cell-ids', methods=['POST'])
-def wkt_to_cell_ids():
-    data = json.loads(request.data.decode('utf-8'))
-    field_wkt = data.get('wkt')
-    res_level = data.get('level')
-
-    poly = loads(field_wkt)
-
-    longs, lats = poly.exterior.coords.xy
-    longs, lats = longs.tolist(), lats.tolist()
-
-    min_level = res_level
-    max_level = res_level
-    r = s2.RegionCoverer()
-    r.min_level = min_level
-    r.max_level = max_level
-
-    lb_lat = min(lats)
-    ub_lat = max(lats)
-    lb_lon = min(longs)
-    ub_lon = max(longs)
-
-    lb = s2.LatLng.from_degrees(lb_lat, lb_lon)
-    ub = s2.LatLng.from_degrees(ub_lat, ub_lon)
-    cids = r.get_covering(s2.LatLngRect.from_point_pair(lb, ub))
-    s2_token_list = []
-    for cid in cids:
-        s2_token_list.append(cid.to_token())
-
-    return jsonify({
-        "s2 token list": s2_token_list,
-    })
 
 
 @app.route('/kml-to-wkt', methods=['POST'])
@@ -92,5 +29,31 @@ def convert_kml_to_wkt():
     wkt = poly.wkt
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/register-field-boundary', methods=['POST'])
+def register_field_boundary():
+    data = json.loads(request.data.decode('utf-8'))
+    field_wkt = data.get('wkt')
+    resolution_level = data.get('resolution_level')
+
+    # get the L13 indices
+    # s2_index__L13_list is a list of tokens(hex encoded version of the cell id)
+    s2_index__l13_list = S2Service.wkt_to_cell_ids(field_wkt, resolution_level)
+
+    # generate the geo_id only for `s2_index__l13_list`
+    geo_id = Utils.generate_geo_id(s2_index__l13_list)
+
+    # lookup the database to see if geo id already exists
+    geo_id_exists = Utils.lookup_geo_ids(geo_id)
+
+    # if geo id not registered, register it in the database
+    if not geo_id_exists:
+        Utils.register_field_boundary(geo_id, s2_index__l13_list, resolution_level)
+        return jsonify({
+            "Message": "Field Boundary registered successfully.",
+            "GEO ID": geo_id,
+            "S2 Cell Tokens": s2_index__l13_list
+        })
+    else:
+        return jsonify({
+            "Message": "Field Boundary already registered."
+        })
